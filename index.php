defcf9be9a0139a0b973e0568e216788773b5631
Andrew Richard Martinez <andrew.r.martinez@Andrews-MacBook-Pro-2.local>
2018-08-23 12:22:47 -0600

    TKT01 - added cdn(css/js) for bootstrap

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<?php echo '<p>Hello World</p>'; ?> 
-<?php echo '<p>Is this thing on?</p>'; ?> 
-<?php echo '<a href="new.html">Click this for a new page</a>'; ?> 
-<?php phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <title>Test page</title>
+</head>
+<body>
+    <h1>Welcome to our Test Application</h1>
+    <p>Let's see if this page will actually render</p>
+</body>
+</html>
